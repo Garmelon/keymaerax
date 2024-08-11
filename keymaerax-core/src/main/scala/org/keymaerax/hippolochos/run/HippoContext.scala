6abd00e26fbca20921a17b7a19a8faed8ddca8a9
Joscha Mennicken <keymaerax@plugh.de>
2024-08-11 19:38:34 +0200

    Define types of hippo tactics

---
+++ keymaerax-core/src/main/scala/org/keymaerax/hippolochos/run/HippoContext.scala
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) Carnegie Mellon University, Karlsruhe Institute of Technology.
+ * See LICENSE.txt for the conditions of this license.
+ */
+
+package org.keymaerax.hippolochos.run
+
+class HippoContext

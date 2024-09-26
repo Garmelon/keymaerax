4b30c913852b1bf8d67fa3fa7c9e35ead4252ded
Joscha Mennicken <keymaerax@plugh.de>
2024-09-26 20:43:24 +0200

    TODO Test

---
+++ keymaerax-core/src/main/scala/org/keymaerax/hippolochos/Test.scala
@@ -0,0 +1,208 @@
+/*
+ * Copyright (c) Carnegie Mellon University, Karlsruhe Institute of Technology.
+ * See LICENSE.txt for the conditions of this license.
+ */
+
+package org.keymaerax.hippolochos
+
+import org.keymaerax.btactics.Z3ToolProvider
+import org.keymaerax.core.{Formula, PrettyPrinter, Sequent, Skolemize, SuccPos}
+import org.keymaerax.hippolib.core.{CoreAxioms, CoreRule, QE, RewriteAt, RewriteAtU, US}
+import org.keymaerax.hippolib.primitive.BidiBackward
+import org.keymaerax.hippolochos.cache.{HippoProofFsCache, LruCache, ProvableFsCache}
+import org.keymaerax.hippolochos.proof.{DerivedHippoProof, HippoProof}
+import org.keymaerax.hippolochos.run.HippoContext
+import org.keymaerax.hippolochos.tools.ExprPath
+import org.keymaerax.parser.StringConverter.StringToStringConverter
+import org.keymaerax.tools.install.ToolConfiguration
+import org.keymaerax.{Configuration, FileConfiguration}
+
+import java.nio.file.Path
+
+object Test {
+  def sequent(formula: Formula): Sequent = Sequent(ante = IndexedSeq(), succ = IndexedSeq(formula))
+  def sequent(formula: String): Sequent = sequent(formula.asFormula)
+
+  /**
+   * {{{
+   * §  [x:=*;][?x>0;][x:=x+1;]x>1
+   * §  --------------------------
+   * §    [x:=*;?x>0;x:=x+1;]x>1
+   * }}}
+   */
+  def simpleComposition(ctx: HippoContext): HippoProof = {
+    // §                           *
+    // §             ----------------------------- [;] compose
+    // §             [a;b;]p(||) <-> [a;][b;]p(||)
+    // §  --------------------------------------------------- US
+    // §  [x:=*;?x>0;x:=x+1;]x>1 <-> [x:=*;][?x>0;x:=x+1;]x>1
+    val eq1 = ctx.forwardJoin(
+      US(
+        "a;".asProgram -> "x:=*;".asProgram,
+        "b;".asProgram -> "?x>0;x:=x+1;".asProgram,
+        "p(||)".asFormula -> "x>1".asFormula,
+      ),
+      CoreAxioms.composeb(ctx),
+    )
+
+    // §                      *
+    // §        ----------------------------- [;] compose
+    // §        [a;b;]p(||) <-> [a;][b;]p(||)
+    // §  ----------------------------------------- US
+    // §  [?x>0;x:=x+1;]x>1 <-> [?x>0;][x:=x+1;]x>1
+    //
+    // This time with the chaining API
+    val eq2 = ctx
+      .chain(CoreAxioms.composeb(ctx))
+      .forward(US(
+        "a;".asProgram -> "?x>0;".asProgram,
+        "b;".asProgram -> "x:=x+1;".asProgram,
+        "p(||)".asFormula -> "x>1".asFormula,
+      ))
+      .proof
+
+    // Combine everything:
+    //
+    // §  [x:=*;][?x>0;][x:=x+1;]x>1   eq2
+    // §  -------------------------------- RewriteAt
+    // §      [x:=*;][?x>0;x:=x+1;]x>1                 eq1
+    // §      -------------------------------------------- RewriteAt
+    // §                 [x:=*;?x>0;x:=x+1;]x>1
+    ctx
+      .chain(Sequent(ante = IndexedSeq(), succ = IndexedSeq("[x:=*;][?x>0;][x:=x+1;]x>1".asFormula)))
+      .forwardJoin(RewriteAt(ExprPath(1)), eq2)
+      .forwardJoin(RewriteAt(ExprPath()), eq1)
+      .proof
+  }
+
+  /**
+   * {{{
+   * §  [x:=*;][?x>0;][x:=x+1;]x>1
+   * §  -------------------------- RewriteAtU(.1, composeb)
+   * §   [x:=*;][?x>0;x:=x+1;]x>1
+   * §   ------------------------ RewriteAtU(., composeb)
+   * §    [x:=*;?x>0;x:=x+1;]x>1
+   * }}}
+   */
+  def simpleComposition2(ctx: HippoContext): HippoProof = ctx
+    .chain(Sequent(ante = IndexedSeq(), succ = IndexedSeq("[x:=*;][?x>0;][x:=x+1;]x>1".asFormula)))
+    .forward(RewriteAtU(ExprPath(1), CoreAxioms.composeb(ctx)))
+    .forward(RewriteAtU(ExprPath(), CoreAxioms.composeb(ctx)))
+    .proof
+
+  /**
+   * {{{
+   * §                 *
+   * §           ------------- QE
+   * §           |- x>0->x+1>1
+   * §       -------------------- [:=] assign
+   * §       |- x>0->[x:=x+1;]x>1
+   * §      ---------------------- [?] test
+   * §      |– [?x>0;][x:=x+1;]x>1
+   * §      ---------------------- [;] compose
+   * §       |– [?x>0;x:=x+1;]x>1
+   * §  ------------------------------ skolemize
+   * §  |– \forall x [?x>0;x:=x+1;]x>1
+   * §  ------------------------------ [:*] assign nondet
+   * §    |– [x:=*;][?x>0;x:=x+1;]x>1
+   * §    --------------------------- [;] compose
+   * §     |– [x:=*;?x>0;x:=x+1;]x>1
+   * }}}
+   */
+  def simpleComposition3(ctx: HippoContext): HippoProof = ctx
+    .chain("==> [x:=*;?x>0;x:=x+1;]x>1".asSequent)
+    .backward(RewriteAtU(ExprPath(), CoreAxioms.composeb(ctx)))
+    .backward(RewriteAtU(ExprPath(), CoreAxioms.randomb(ctx)))
+    .backward(CoreRule(Skolemize(SuccPos(0))))
+    .backward(RewriteAtU(ExprPath(), CoreAxioms.composeb(ctx)))
+    .backward(RewriteAtU(ExprPath(), CoreAxioms.testb(ctx)))
+    .backward(RewriteAtU(ExprPath(1), CoreAxioms.assignbAxiom(ctx)))
+    .backward(QE)
+    .proof
+
+  /**
+   * {{{
+   * §                 *
+   * §           ------------- QE
+   * §           |- x>0->x+1>1
+   * §       -------------------- [:=] assign
+   * §       |- x>0->[x:=x+1;]x>1
+   * §      ---------------------- [?] test
+   * §      |– [?x>0;][x:=x+1;]x>1
+   * §      ---------------------- [;] compose
+   * §       |– [?x>0;x:=x+1;]x>1
+   * §  ------------------------------ skolemize
+   * §  |– \forall x [?x>0;x:=x+1;]x>1
+   * §  ------------------------------ [:*] assign nondet
+   * §    |– [x:=*;][?x>0;x:=x+1;]x>1
+   * §    --------------------------- [;] compose
+   * §     |– [x:=*;?x>0;x:=x+1;]x>1
+   * }}}
+   */
+  def simpleComposition4(ctx: HippoContext): HippoProof = ctx
+    .chain("==> x>0->x+1>1".asSequent)
+    .backward(QE)
+    // .forward(Forward(RewriteAt(ExprPath(1)), "==> x>0->[x:=x+1;]x>1".asSequent))
+    // .backward(Unify(CoreAxioms.assignbAxiom))
+    .forward(BidiBackward(RewriteAtU(ExprPath(1), CoreAxioms.assignbAxiom(ctx)), "==> x>0->[x:=x+1;]x>1".asSequent))
+    .forward(RewriteAtU(ExprPath(), CoreAxioms.testb(ctx)))
+    .forward(RewriteAtU(ExprPath(), CoreAxioms.composeb(ctx)))
+    .forward(BidiBackward(CoreRule(Skolemize(SuccPos(0))), "==> \\forall x [?x>0;x:=x+1;]x>1".asSequent))
+    .forward(RewriteAtU(ExprPath(), CoreAxioms.randomb(ctx)))
+    .forward(RewriteAtU(ExprPath(), CoreAxioms.composeb(ctx)))
+    .proof
+
+  val simpleComposition: DerivedHippoProof = DerivedHippoProof("==> [x:=*;?x>0;x:=x+1;]x>1".asSequent) { ctx =>
+    ctx
+      .chain("==> x>0->x+1>1".asSequent)
+      .backward(QE)
+      .forward(BidiBackward(RewriteAtU(ExprPath(1), CoreAxioms.assignbAxiom(ctx)), "==> x>0->[x:=x+1;]x>1".asSequent))
+      .forward(RewriteAtU(ExprPath(), CoreAxioms.testb(ctx)))
+      .forward(RewriteAtU(ExprPath(), CoreAxioms.composeb(ctx)))
+      .forward(BidiBackward(CoreRule(Skolemize(SuccPos(0))), "==> \\forall x [?x>0;x:=x+1;]x>1".asSequent))
+      .forward(RewriteAtU(ExprPath(), CoreAxioms.randomb(ctx)))
+      .forward(RewriteAtU(ExprPath(), CoreAxioms.composeb(ctx)))
+      .proof
+  }
+
+  def main(args: Array[String]): Unit = {
+    Configuration.setConfiguration(FileConfiguration)
+    PrettyPrinter.setPrinter(org.keymaerax.parser.KeYmaeraXPrettyPrinter.pp)
+
+    val z3ToolProvider = Z3ToolProvider(ToolConfiguration(z3Path = Some("/home/joscha-nixos/stud/keymaerax/z3")))
+    z3ToolProvider.init()
+
+    val ctx = new HippoContext(
+      toolProvider = z3ToolProvider,
+      toolCache =
+        new ProvableFsCache(Path.of("/home/joscha-nixos/stud/keymaerax/cache/tool")).behind(new LruCache(1000)),
+      derivedCache =
+        new HippoProofFsCache(Path.of("/home/joscha-nixos/stud/keymaerax/cache/derived")).behind(new LruCache(1000)),
+    )
+
+    try simpleComposition3(ctx)
+    catch { case e: HippoException => println(e.format) }
+
+//    val proof = ctx.derived(simpleComposition)
+//
+//    printProof(proof)
+//    println()
+//    println(proof)
+//
+//    val provable = ctx.provableFromLocalProof(proof)
+//    println()
+//    println(provable)
+//
+//    val error = SourceFile("first\nsecond\nthird\nfourth\nfifth")
+//      .Slice(8, 23)
+//      .formatError(error = "oopsie woopsie, something's wrong", label = "probably riiiight here")
+//    println()
+//    println(error)
+  }
+
+  private def printProof(proof: HippoProof): Unit = {
+    for (premise <- proof.premises) println(s"${if (premise.mustBeProved) "! " else "  "}${premise.sequent}")
+    println("-----------------------------------------")
+    println(s"  ${proof.conclusion}")
+  }
+}

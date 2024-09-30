00dcf75c6f5a312ffbfda45b209ba7486ec57cfe
Joscha Mennicken <keymaerax@plugh.de>
2024-09-30 13:58:12 +0200

    Extract FileInterpreter into separate file
    It no longer is an inner class of HippoInterpreterContext.

---
+++ keymaerax-core/src/main/scala/org/keymaerax/hippolang/interpret/HippoInterpreterContext.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) Carnegie Mellon University, Karlsruhe Institute of Technology.
+ * See LICENSE.txt for the conditions of this license.
+ */
+
+package org.keymaerax.hippolang.interpret
+
+import org.keymaerax.btactics.ToolProvider
+import org.keymaerax.hippolang.HippoConversions._
+import org.keymaerax.hippolang.HippoValue
+import org.keymaerax.hippolang.namespace.{ImmutableNamespace, MutableNamespace}
+import org.keymaerax.hippolang.parse.{HippoParser, SourceFile}
+import org.keymaerax.hippolochos.run.HippoContext
+
+import java.nio.file.{Files, Path}
+
+case class HippoInterpreterContext(ctx: HippoContext, env: Option[ImmutableNamespace] = None) {
+  def run(file: Path): (HippoValue, ImmutableNamespace) = {
+    val absFile = file.toAbsolutePath
+    val code = Files.readString(absFile)
+    run(SourceFile(code, Some(absFile)))
+  }
+
+  def run(source: SourceFile): (HippoValue, ImmutableNamespace) = {
+    val program = HippoParser.parse(source).toHExpr
+    val fileInterpreter = new FileInterpreter(this, ctx, source.path)
+    val value = fileInterpreter.eval(new MutableNamespace(env), program)
+    val namespace = fileInterpreter.exported.freeze
+    (value, namespace)
+  }
+}
+
+object HippoInterpreterContext {
+  def withCacheDir(
+      toolProvider: ToolProvider,
+      cacheDir: Path,
+      env: Option[ImmutableNamespace] = None,
+  ): HippoInterpreterContext = {
+    val ctx = HippoContext.withCacheDir(toolProvider, cacheDir)
+    new HippoInterpreterContext(ctx, env)
+  }
+}

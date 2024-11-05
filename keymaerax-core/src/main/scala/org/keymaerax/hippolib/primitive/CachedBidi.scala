70fdb38d22e2d6dbb21b088cc140622c73da7bea
Joscha Mennicken <keymaerax@plugh.de>
2024-11-05 18:36:53 +0100

    Add Cached* wrapper tactics

---
+++ keymaerax-core/src/main/scala/org/keymaerax/hippolib/primitive/CachedBidi.scala
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Carnegie Mellon University, Karlsruhe Institute of Technology.
+ * See LICENSE.txt for the conditions of this license.
+ */
+
+package org.keymaerax.hippolib.primitive
+
+import org.keymaerax.core.Sequent
+import org.keymaerax.hippolochos.proof.HippoProof
+import org.keymaerax.hippolochos.run.HippoContext
+import org.keymaerax.hippolochos.tools.Hash
+import org.keymaerax.hippolochos.{BackwardTactic, ForwardTactic}
+
+case class CachedBidi(tactic: ForwardTactic with BackwardTactic) extends ForwardTactic with BackwardTactic {
+  override def hash: Hash = tactic.hash
+
+  override def runForward(ctx: HippoContext, premises: IndexedSeq[Sequent]): HippoProof = ctx
+    .cachedForward(tactic, premises)
+
+  override def runBackward(ctx: HippoContext, conclusion: Sequent, premises: Map[Int, Sequent]): HippoProof = ctx
+    .cachedBackward(tactic, conclusion, premises)
+}

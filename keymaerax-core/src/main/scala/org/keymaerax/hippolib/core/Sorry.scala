cea81204fce96f5e2ba49be3b582467d775054f2
Joscha Mennicken <keymaerax@plugh.de>
2024-11-15 13:47:55 +0100

    Add Sorry tactic

---
+++ keymaerax-core/src/main/scala/org/keymaerax/hippolib/core/Sorry.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Carnegie Mellon University, Karlsruhe Institute of Technology.
+ * See LICENSE.txt for the conditions of this license.
+ */
+
+package org.keymaerax.hippolib.core
+
+import org.keymaerax.core.{Sequent, True}
+import org.keymaerax.hippolochos.proof.{HippoPremise, HippoProof}
+import org.keymaerax.hippolochos.run.HippoContext
+import org.keymaerax.hippolochos.tools.Hash
+import org.keymaerax.hippolochos.{BackwardTactic, ForwardTactic}
+
+import scala.collection.SortedMap
+
+case class Sorry(conclusion: Option[Sequent] = None, premises: SortedMap[Int, Sequent] = SortedMap.empty)
+    extends ForwardTactic with BackwardTactic {
+
+  override lazy val hash: Hash = Hash
+    .start
+    .digest[this.type]
+    .digestOpt(conclusion)(_.digest(_))
+    .digestMap(premises)(_.digest(_).digest(_))
+    .build
+
+  override def runForward(ctx: HippoContext, premises: IndexedSeq[Sequent]): HippoProof = {
+    // TODO Nicer error handling
+    require(this.conclusion.isDefined)
+    val conclusion = this.conclusion.get
+    for ((i, premise) <- this.premises) require(premises(i) == premise)
+    ctx.sorry(conclusion, premises.map(HippoPremise.locallySound))
+  }
+
+  override def runBackward(ctx: HippoContext, conclusion: Sequent, premises: Map[Int, Sequent]): HippoProof = {
+    // TODO Nicer error handling
+    for (conc <- this.conclusion) require(conclusion == conc)
+    for ((i, premise) <- this.premises) require(premises(i) == premise)
+    val allPremises = this.premises ++ premises
+    val maxI = allPremises.keys.maxOption.getOrElse(-1)
+    val trueSequent = Sequent(IndexedSeq(), IndexedSeq(True)) // "==> true"
+    val premisesList = (0 to maxI).map(allPremises.getOrElse(_, trueSequent))
+    ctx.sorry(conclusion, premisesList.map(HippoPremise.locallySound))
+  }
+}
